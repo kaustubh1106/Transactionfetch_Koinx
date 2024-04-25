8c346b49799886bd526de2e2187fdf2df8133b88
kaustubh <kaustubhsharma1601@gmail.com>
2024-04-26 03:03:49 +0530

    changed mongo db url to env

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const userRouter = require("./routes/transactionRoutes.js")
 
 const app = express();
 
+const PORT =process.env.PORT || 3000
+
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-connectMongoDB(`mongodb+srv://${process.env.USERNAME}:${process.env.DB_PASS}@cluster0.pch41pq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+connectMongoDB(`${process.env.URL}`)
     .then(() => { console.log("database connected") })
     .catch((e) => { console.log("monggo error", e) })
 
@@ -20,7 +22,9 @@ setInterval(price, 600000); // TASK 2: FETCH ETH PRICE EVERY TEN MINUTE
 
 
 app.use("/", userRouter)
+ 
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log('server started');
 })
